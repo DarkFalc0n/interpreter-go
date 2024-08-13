7975a99554c438522dddb53a7aa66492ae0f787b
DarkFalc0n <pratyayroy20@gmail.com>
2024-08-13 23:20:00 +0530

    fix: correct decimal behaviour

--- internal/token/number.go
+++ internal/token/number.go
@@ -2,10 +2,19 @@ package token
 
 func GetNumber(index *int, fileContents *[]byte, line *int) string {
 	var str string
+	var didScanDecimal bool = false
 	for i := *index; i < len(*fileContents); i++ {
-		if (isDigit((*fileContents)[i]) || (*fileContents)[i] == '.') {
+		if (isDigit((*fileContents)[i]) ) {
 			str += string((*fileContents)[i])
-		} else {
+		} else if (*fileContents)[i] == '.'{
+			if didScanDecimal {
+				*index = i - 1
+				break
+			} else {
+				str += string((*fileContents)[i])
+				didScanDecimal = true
+			}
+		}else {
 			*index = i - 1
 			break;
 		}
